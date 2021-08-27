93c68ca9c3e7f21d1fe89608084c447ea1873fcb
Obetta <francischassobetta@gmail.com>
2021-08-27 13:59:27 +0100

    Define redux actions and reducers

---
+++ src/reducers/books.js
@@ -0,0 +1,12 @@
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions';
+
+export default function booksReducer(state = [], action) {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+}
