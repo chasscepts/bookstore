a49bd45a30ce6718ba93435a1e7cde5f5e5b47b4
Obetta <francischassobetta@gmail.com>
2021-08-27 13:31:16 +0100

    Prepare directory structure

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Welcome to magic bookstore</h1>;
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Magic Bookstore</h1>;
+}
+
+export default App;
