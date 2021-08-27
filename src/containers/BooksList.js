3440c36152a6d1c95a03638ad670f64551226da6
Obetta <francischassobetta@gmail.com>
2021-08-27 13:53:43 +0100

    Implement BookList component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,3 +1,22 @@
+//  import { useSelector } from 'react-redux';
+import Book from '../components/Book';
+
 export default function BooksList() {
-  return <></>;
+  //  const books = useSelector((state) => state.books);
+  const books = [];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map((book) => <Book key={book.id} book={book} />)}
+      </tbody>
+    </table>
+  );
 }
